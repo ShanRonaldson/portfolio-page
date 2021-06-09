5a67a99b959f22761aa4543ed0a8e0db58aeba9d
Shannon Schrauwen <63632791+ShanSchrauwen@users.noreply.github.com>
2021-06-09 12:42:56 +0300

    react router & favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 
+
+
 export function Header(){
     return(
         <div className="header">
             <header className="header--main">Hello I am a header</header>
+      
         </div>
     )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,1 +1,39 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Contact } from "../base/ContactPage";
+import { Home } from "../base/HomePage";
+import { Portfolio } from "../base/PortfolioPage";
 
+export function Nav() {
+  return (
+    <Router>
+      <div className="nav--Router">
+        <ul className="nav--Router__ul">
+          <li className="nav--Router__li">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav--Router__li">
+            <Link to="/portfolio">Portfolio</Link>
+          </li>
+          <li className="nav--Router__li">
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/portfolio">
+            <Portfolio />
+          </Route>
+
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
