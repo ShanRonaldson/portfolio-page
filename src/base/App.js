58a9edc396ccb40a125260b176b6402d693483bd
Shannon Schrauwen <63632791+ShanSchrauwen@users.noreply.github.com>
2021-06-09 10:59:08 +0300

    creation of header and making SCSS mixins

--- src/base/App.js
+++ src/base/App.js
@@ -1,22 +1,9 @@
-import logo from "../images/logo.svg";
+import {Header} from "../components/Header";
 
-function App() {
+export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+export function Header(){
+    return(
+        <div className="header">
+            <header className="header--main">Hello I am a header</header>
+        </div>
+    )
+}
+
+export default Header;
