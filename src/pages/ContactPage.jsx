ff596642a56cd238fdad1048dad8429aa1f16daa
Shannon Schrauwen <63632791+ShanSchrauwen@users.noreply.github.com>
2021-06-11 20:49:08 +0300

    media queries for contact + home page + components

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -4,17 +4,6 @@ export function Contact() {
   return (
     <div className="contact--content margin-top-med margin-bottom-large">
       <Form />
-
-      <div className="contact--info">
-          <h3 className="contact--info__heading">
-              Contact Information
-          </h3>
-
-            <p className="contact--info__para">
-                Helsinki, Finland
-            </p>
-
-      </div>
     </div>
   );
 }
