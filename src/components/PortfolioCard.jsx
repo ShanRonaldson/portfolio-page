ba9ec147f6f0238476c3522f661d7f6bb4aebcf0
Shannon Schrauwen <63632791+ShanSchrauwen@users.noreply.github.com>
2021-06-13 21:37:25 +0300

    portfolio card animations

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -3,19 +3,28 @@ export function PortfolioCard(props) {
     <div className="portfolio-card">
       <h4 className="portfolio-card__heading">{props.heading}</h4>
 
-      <p className="portfolio-card__description">
+      {/* <div className="portfolio-card__description">
         This is the description of the website.
-      </p>
+      </div> */}
 
-      <a href={props.url} className="portfolio-card__link" target="_blank" rel="noreferrer">
-        <img
-          src={props.portfolioImg}
-          alt={props.alt}
-          className="portfolio-card__img"
-        />
+      <a
+        href={props.url}
+        className="portfolio-card__link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <figure className="portfolio-card__overlay">
+          <img
+            src={props.portfolioImg}
+            alt={props.alt}
+            className="portfolio-card__img"
+          />
+
+          <figcaption className="portfolio-card__caption">
+            This is the description of the website
+          </figcaption>
+        </figure>
       </a>
-
-
     </div>
   );
 }
