2e22a186f039bd7ca0f07100a8029a59e3ccb89b
Shannon Schrauwen <63632791+ShanSchrauwen@users.noreply.github.com>
2021-06-14 14:19:33 +0300

    removal of placeholders. proper info added

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -4,52 +4,59 @@ export function Portfolio() {
   return (
     <div className="portfolio margin-top-med margin-bottom-large">
       <PortfolioCard
-        heading="First guess"
-        url="https://shanschrauwen.github.io/"
-        portfolioImg="./portfolio-imgs/portfolio-placeholder.png"
-        alt="alt tag"
+        heading="Natours - Advanced CSS course"
+        url="https://shanschrauwen.github.io/Natours_Jonas_Course/"
+        portfolioImg="./portfolio-imgs/natours_site.jpg"
+        alt="Screenshot of front page of website"
       />
 
       <PortfolioCard
-        heading="Second guess"
-        url="https://shanschrauwen.github.io/"
-        portfolioImg="./portfolio-imgs/portfolio-placeholder.png"
-        alt="alt tag"
+        heading="Trillo Site - Advanced CSS course"
+        url="https://shanschrauwen.github.io/starter-Trillo-Jonas/"
+        portfolioImg="./portfolio-imgs/trillo_site.jpg"
+        alt="Screenshot of front page of website"
       />
 
       <PortfolioCard
-        heading="Third guess"
-        url="https://shanschrauwen.github.io/"
-        portfolioImg="./portfolio-imgs/portfolio-placeholder.png"
-        alt="alt tag"
+        heading="Blog site - Web Development Bootcamp"
+        url="https://udemy-course-blogsite.herokuapp.com/"
+        portfolioImg="./portfolio-imgs/blog_site.jpg"
+        alt="Screenshot of front page of website"
       />
 
       <PortfolioCard
-        heading="Fifth guess"
-        url="https://shanschrauwen.github.io/"
-        portfolioImg="./portfolio-imgs/portfolio-placeholder.png"
-        alt="alt tag"
+        heading="To Do list site - Web Development Bootcamp"
+        url="https://udemy-course-todolist.herokuapp.com/"
+        portfolioImg="./portfolio-imgs/todo_list_site.jpg"
+        alt="Screenshot of front page of website"
       />
 
       <PortfolioCard
-        heading="Sixth guess"
-        url="https://shanschrauwen.github.io/"
-        portfolioImg="./portfolio-imgs/portfolio-placeholder.png"
-        alt="alt tag"
+        heading="Simon Says - Web Development Bootcamp"
+        url="https://shanschrauwen.github.io/Simon-Says-Challenge/"
+        portfolioImg="./portfolio-imgs/simon_says_site.jpg"
+        alt="Screenshot of front page of website"
       />
 
       <PortfolioCard
-        heading="Seventh guess"
-        url="https://shanschrauwen.github.io/"
-        portfolioImg="./portfolio-imgs/portfolio-placeholder.png"
-        alt="alt tag"
+        heading="Drum Kit - Web Development Bootcamp"
+        url="https://shanschrauwen.github.io/Drum-Kit/"
+        portfolioImg="./portfolio-imgs/drum_kit_site.jpg"
+        alt="Screenshot of front page of website"
       />
 
       <PortfolioCard
-        heading="Eighth guess"
-        url="https://shanschrauwen.github.io/"
-        portfolioImg="./portfolio-imgs/portfolio-placeholder.png"
-        alt="alt tag"
+        heading="Newsletter sign-up - Web Development Bootcamp"
+        url="https://udemy-course-email-signup.herokuapp.com/"
+        portfolioImg="./portfolio-imgs/newletter_signup_site.jpg"
+        alt="Screenshot of front page of website"
+      />
+
+      <PortfolioCard
+        heading="Dice Challenge site - Web Development Bootcamp"
+        url="https://shanschrauwen.github.io/Dice-Challenge/"
+        portfolioImg="./portfolio-imgs/dice_site.jpg"
+        alt="Screenshot of front page of website"
       />
     </div>
   );
